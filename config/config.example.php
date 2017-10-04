6090d80e2db28ba3bde404ca2f5f958ad54d83f2
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-04 09:55:01 +0800

    add database.sql

--- config/config.example.php
+++ config/config.example.php
@@ -4,3 +4,10 @@ $C['token'] = 'token';
 $C['chat_id'] = 'chat_id';
 
 $C["allowsapi"] = array("cli");
+
+$C["DBhost"] = 'localhost';
+$C['DBname'] = '';
+$C['DBuser'] = '';
+$C['DBpass'] = '';
+$C['DBTBprefix'] = 'Adminbacklog';
+$G["db"] = new PDO ('mysql:host='.$C["DBhost"].';dbname='.$C["DBname"].';charset=utf8mb4', $C["DBuser"], $C["DBpass"]);
