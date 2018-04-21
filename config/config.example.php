256db348655d36d7c76546825c393957e0a964fa
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-04-21 11:21:30 +0800

    update ChatDescription

--- config/config.example.php
+++ config/config.example.php
@@ -25,6 +25,7 @@ $C["typelist"] = [
 	"epnone" => ["epnone"],
 	"epsemi" => ["epsemi"],
 	"revoke" => ["revoke"],
+	"rfcuham" => ["rfcuham"],
 	"rfcu" => ["rfcu"],
 	"rfpp" => ["rfpp"],
 	"rfr" => ["rfrflood", "rfrautoreview", "rfrawb", "rfrconfirm", "rfripbe", "rfrmms", "rfrpatrol", "rfrrollback"],
@@ -34,17 +35,14 @@ $C["typelist"] = [
 	"uc" => ["uc"],
 	"unblock" => ["unblock"],
 	"vip" => ["vip"],
-	"sum" => ["afdb", "affp", "csd", "cv", "drv", "epfull", "epnone", "epsemi", "revoke", "rfcu", "rfpp", "rfr", "rm", "rrd", "uaa", "uc", "unblock", "vip"]
+	"sum" => ["afdb", "affp", "csd", "cv", "drv", "epfull", "epnone", "epsemi", "revoke", "rfcuham", "rfpp", "rfr", "rm", "rrd", "uaa", "uc", "unblock", "vip"]
 ];
-$C["ChatDescription"] = "中文維基百科管理員積壓工作直播
-目前有%sum%項積壓工作
-#速刪(%csd%) #hangon #繁簡 | #RRD(%rrd%) | #侵權(%cv%)
+$C["ChatDescription"] = "中文維基百科管理員積壓工作直播，共有%sum%項
+#速刪(%csd%) | #RRD(%rrd%) | #侵權(%cv%)
 #VIP(%vip%) | #RFPP(%rfpp%) | #封禁申訴(%unblock%) | #UAA(%uaa%)
-#存廢積壓(%afdb%) | #存廢覆核(%drv%) #OH #新申請 | #除權(%revoke%)
-#編輯請求(%ep%) #EFP(%epfull%) #ESP(%epsemi%) #ENP(%epnone%) | #RFR(%rfr%)
-#AFFP(%affp%) | #移動請求(%rm%) | #更名(%uc%) | #RFCU(%rfcu%)
-
-定時刪討論，重要內容自行備份，以保持乾淨";
+#存廢積壓(%afdb%) | #存廢覆核(%drv%) | #除權(%revoke%)
+#編輯請求(%ep%) | #RFR(%rfr%) | #AFFP(%affp%) | #更名(%uc%)
+#移動請求(%rm%) | #RFCU(%rfcu%) | #RFCUHAM(%rfcuham%)";
 
 $C["autodellimit"] = [
 	["text", "/.*/", 60*60*24],
