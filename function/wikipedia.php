d462da21bf8358bbc91b603fdb0e93e7d09aadb1
xiplus <huangxuanyuxiplus@gmail.com>
2019-09-30 15:08:31 +0800

    Fix RRD titleregex

--- function/wikipedia.php
+++ function/wikipedia.php
@@ -91,7 +91,7 @@ class RRDHandler extends WikipediaAdminbacklogBasepage {
 		['/({{Revdel)/', '%s$1'],
 	];
 	private $statusregex = '/\|status\s*=\s*(|OH|新申請.*?|<!--不要修改本参数-->)\n/';
-	private $titleregex = '/\|article = (.+?) *\n/';
+	private $titleregex = '/\|article = *(.+?) *\n/';
 	private $requesterregex = '/{{Revdel[\s\S]*?}}\n.*?\[\[(?:(?:User(?:[ _]talk)?|U|UT|用户|用戶|使用者):|Special:(?:(?:Contributions|Contribs)|(?:用户|用戶|使用者)?(?:贡献|貢獻))\/)([^\/|\]]*)/';
 
 	public function __construct() {
